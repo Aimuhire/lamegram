1b6e72304aaa00148e5fa5a4543bd9405af825e8
Arsène I. Muhire <marsanline@gmail.com>
2021-06-30 20:59:34 +0200

    Add compiled undle

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.use('/', express.static('.'))
+app.use('/lamegram/', express.static('.'))
 app.get("*", (req, res) => res.sendFile(__dirname + "/index.html"))
 app.listen(port, () => console.log("Server started"))
